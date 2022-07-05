0ce024adfb2853aad9e1dee24cf62016e37da052
Salomon Diei <salomondiei08@gmail.com>
2022-07-05 11:19:55 +0000

    Done

--- assets/js/ollie.js
+++ assets/js/ollie.js
@@ -62,3 +62,24 @@ $('#owl-testmonial').owlCarousel({
     nav: true,
     dots: false
 })
+
+function download(fileName, fileUrl) {
+    var a = document.createElement("a");
+    a.href = fileUrl;
+    a.setAttribute("download", fileName);
+    a.click();
+}
+
+
+for(var i = 0; i < document.getElementsByClassName('download-btn').length; i++)
+{
+    document.getElementsByClassName('download-btn')[i].addEventListener("click", function(){
+        // Generate download of hello.txt file with some content
+        
+        var fileUrl = "assets/files/Stic-Congo.pdf";
+        var filename = "Stic-Congo.pdf";
+        
+        download(filename, fileUrl);
+    }, false)
+}
+// Start file download.
